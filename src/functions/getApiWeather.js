1e7b0017083e71b18382fa9a66dd83fd06f16b33
estefanoo <estefanocarlos54@gmail.com>
2020-09-28 19:34:41 -0300

    problemas cors - resuelto

--- src/functions/getApiWeather.js
+++ src/functions/getApiWeather.js
@@ -3,13 +3,13 @@
 export const getApiWeather = async(localization) =>{
         
     // busco la zona con las coordenadas
-    const api = await fetch(`https://www.metaweather.com/api/location/search/?${localization}`);
+    const api = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?${localization}`);
     // transformo a json
     const data = await api.json();
     // accedo a la primera ciudad y tomo el valor del "woeid"
     const {woeid} = data[0];
     // busco la temperatura con el valor del "woeid"
-    const weather = await fetch(`https://www.metaweather.com/api/location/${woeid}`);
+    const weather = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}`);
     // transformo a json
     const data2 = await weather.json();
 
