10767f25852082a58684f19cb5b671933406e72b
estefanoo <estefanocarlos54@gmail.com>
2020-09-28 18:57:03 -0300

    Updates

---
+++ precache-manifest.bd8e18465801d127463cf98a4782b546.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a8e1feacd2c20f1b200c280ec8a5ed",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "3a9ba3735836d71986fd",
+    "url": "/weather-app/static/css/main.3358f6aa.chunk.css"
+  },
+  {
+    "revision": "c4c3eed57e4e677eed8b",
+    "url": "/weather-app/static/js/2.bbbfdf1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.bbbfdf1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a9ba3735836d71986fd",
+    "url": "/weather-app/static/js/main.e5af0c3f.chunk.js"
+  },
+  {
+    "revision": "d37aae7dd785effe0b90",
+    "url": "/weather-app/static/js/runtime-main.cd47ba67.js"
+  },
+  {
+    "revision": "2b5e332a260ca22a27156a9dde7c8236",
+    "url": "/weather-app/static/media/location.2b5e332a.svg"
+  }
+]);
